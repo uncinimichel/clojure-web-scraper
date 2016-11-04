df3e864a16ec223706bad302288a3cc2b7bc4a17
Michel Uncini <uncini.michel@gmail.com>
2016-11-04 06:22:15 +0000

    parse errors

--- src/clojure_web_scraper/common.clj
+++ src/clojure_web_scraper/common.clj
@@ -10,27 +10,30 @@
 
 (defn transform-text-number
   [text]
-  (if (= (string/lower-case text) "flat")
-    (int 0)
-    (if (re-matches  #"\d+" text)
-      (Integer. text)
-      (if (re-matches #"\d+-(\d+)" text)
-        (Integer. (second (re-matches #"\d+-(\d+)" text)))
-        (int 0)))))
+
+  (if (= (string/lower-case text) "error")
+    (int -1)
+    (if (= (string/lower-case text) "flat")
+      (int 0)
+      (if (re-matches  #"\d+" text)
+        (Integer. text)
+        (if (re-matches #"\d+-(\d+)" text)
+          (Integer. (second (re-matches #"\d+-(\d+)" text)))
+          (int 0))))))
 
 
-(defn hmac
-  "Calculate HMAC signature for given data."
-  [^String key ^String data]
-  (let [hmac-sha1 "HmacSHA256"
-        signing-key (SecretKeySpec. (.getBytes key) hmac-sha1)
-        mac (doto (Mac/getInstance hmac-sha1) (.init signing-key))]
-    (String. (org.apache.commons.codec.binary.Base64/encodeBase64
-              (.doFinal mac (.getBytes data)))
-             "UTF-8")))
+;; (defn hmac
+;;   "Calculate HMAC signature for given data."
+;;   [^String key ^String data]
+;;   (let [hmac-sha1 "HmacSHA256"
+;;         signing-key (SecretKeySpec. (.getBytes key) hmac-sha1)
+;;         mac (doto (Mac/getInstance hmac-sha1) (.init signing-key))]
+;;     (String. (org.apache.commons.codec.binary.Base64/encodeBase64
+;;               (.doFinal mac (.getBytes data)))
+;;              "UTF-8")))
 
 
-(hmac "ciao" "123")
+;; (hmac "ciao" "123")
 
 ;; (http/get "dsadass" (fn [v] (println v))(fn [v] (println "daskljdasksda" v)))
 
@@ -38,8 +41,6 @@
 (defn http-get [url]
   (let [c (chan)]
     (println "Calling this url:" url)
-    (http/get url
-              (fn [r] (put! c r))
-              (fn [err] (put! c err)))
+    (http/get url (fn [r] (put! c r)))
     c))
 
--- src/clojure_web_scraper/core.clj
+++ src/clojure_web_scraper/core.clj
@@ -1,21 +1,29 @@
 (ns clojure-web-scraper.core
   (:require
+    [clojure.string :as string]
     [clojure-web-scraper.common :as common]
     [hickory.core :as hickory]
     [hickory.select :as s]
     [clojure.core.async :as async :refer [go go-loop put! take! <! >! <!! timeout chan alt! go]]))
 
 (def surfing-urls [
-                    { :key "East-Wittering" :url "http://magicseaweed.com/East-Wittering-Surf-Report/14/"}
+                    { :key "East-Wittering" :url "ssshttp://magicseaweed.com/East-Wittering-Surf-Report/14/"}
                     { :key "West-Wittering" :url "http://magicseaweed.com/West-Wittering-Surf-Report/3765/"}
                     { :key "Tamri" :url "http://magicseaweed.com/Tamri-Surf-Report/1086/"}
                     { :key "Haouzia" :url "http://magicseaweed.com/Haouzia-Surf-Report/3118/"}
                     ])
 
 (defn selector-get-waves-size-text [hickory-tree] (-> (s/select (s/class "rating-text") hickory-tree)
+
                                                       first :content first string/trim))
 
 
+(defn parse-response
+  [response]
+  (let [{:keys [body error]} response]
+    (if error
+      "<div class=\"rating-text\">error</div>"
+      body)))
 
 (defn call-all-the-urls-optn
   []
@@ -25,7 +33,7 @@
       (doseq [[url-key c] urls-channels]
         (println "Surfing for: " url-key (-> c
                                          <!
-                                         :body
+                                         parse-response
                                          hickory/parse
                                          hickory/as-hickory
                                          selector-get-waves-size-text
